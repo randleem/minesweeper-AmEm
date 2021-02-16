a3604a64eda25ba0ca4715d09dc4aeac829e5048
randleem <erandle2211@gmail.com>
2021-02-16 14:17:45 +0000

    board set up (we think)

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import Cell from "../Cell/Cell";
 
 function Board({ gameProps }) {
-  const [boardData, setBoardData] = useState(gameProps);
-  const [gameStatus, setGameStatus] = useState(false);
+  let {height, width, mines} = gameProps;
+  const [boardData, setBoardData] = useState(initBoardData(height, width, mines));
+  const [gameStatus, setGameStatus] = useState("Game about to Start");
   const [mineCount, setMineCount] = useState(gameProps.mines);
 
   // Gets initial board data
@@ -56,6 +57,53 @@ function Board({ gameProps }) {
     return Math.floor(Math.random() * 1000 + 1) % dimension;
   }
 
+  /* Helper Functions */
+
+  // get mines
+  function getMines(data) {
+    let mineArray = [];
+
+    data.map(datarow => {
+      return datarow.map((dataitem) => {
+        if (dataitem.isMine) {
+          return mineArray.push(dataitem);
+        }
+      });
+    });
+
+    return mineArray;
+  }
+
+  // get Flags
+  function getFlags(data) {
+    let mineArray = [];
+
+    data.map(datarow => {
+      return datarow.map((dataitem) => {
+        if (dataitem.isFlagged) {
+          return mineArray.push(dataitem);
+        }
+      });
+    });
+
+    return mineArray;
+  }
+
+  // get Hidden cells
+  function getHidden(data) {
+    let mineArray = [];
+
+    data.map(datarow => {
+      return datarow.map((dataitem) => {
+        if (!dataitem.isRevealed) {
+          return mineArray.push(dataitem);
+        }
+      });
+    });
+
+    return mineArray;
+  }
+
   // get number of neighbouring mines for each board cell
   function getNeighbours(data, height, width) {
     let index = 0;
@@ -68,7 +116,7 @@ function Board({ gameProps }) {
           const area = traverseBoard(data[i][j].x, data[i][j].y, data);
           area.map((value) => {
             if (value.isMine) {
-              mine++;
+              return mine++;
             }
           });
           if (mine === 0) {
@@ -128,10 +176,125 @@ function Board({ gameProps }) {
 
     return el;
   }
+
+// reveals the whole board
+function revealBoard() {
+  let updatedData = boardData;
+  updatedData.map((datarow) => {
+    return datarow.map((dataitem) => {
+      return dataitem.isRevealed = true;
+    });
+  });
+  setBoardData(updatedData)
+}
+
+/* reveal logic for empty cell */
+function revealEmpty(x, y, data) {
+  let area = traverseBoard(x, y, data);
+  area.map(value => {
+    if (!value.isFlagged && !value.isRevealed && (value.isEmpty || !value.isMine)) {
+      data[value.x][value.y].isRevealed = true;
+      if (value.isEmpty) {
+        revealEmpty(value.x, value.y, data);
+      }
+    }
+  });
+  return data;
+}
+
+// Handle User Events
+
+function handleCellClick(x, y) {
+
+  // check if revealed. return if true.
+  if (boardData[x][y].isRevealed || boardData[x][y].isFlagged) return null;
+
+  // check if mine. game over if true
+  if (boardData[x][y].isMine) {
+    setGameStatus("You Lost.")
+    revealBoard();
+    alert("game over");
+  }
+
+  let updatedData = boardData;
+  updatedData[x][y].isFlagged = false;
+  updatedData[x][y].isRevealed = true;
+
+  if (updatedData[x][y].isEmpty) {
+    updatedData = revealEmpty(x, y, updatedData);
+  }
+
+  if (getHidden(updatedData).length === boardData.mines) {
+    setMineCount(0);
+    setGameStatus("You Win.")
+    revealBoard();
+    alert("You Win");
+  }
+
+  setBoardData(updatedData);
+  setMineCount(boardData.mines - getFlags(updatedData).length);
+}
+ 
+// right click
+function handleContextMenu(e, x, y) {
+  e.preventDefault();
+  let updatedData = boardData;
+  let mines = mineCount;
+
+  // check if already revealed
+  if (updatedData[x][y].isRevealed) return;
+
+  if (updatedData[x][y].isFlagged) {
+    updatedData[x][y].isFlagged = false;
+    mines++;
+  } else {
+    updatedData[x][y].isFlagged = true;
+    mines--;
+  }
+
+  if (mines === 0) {
+    const mineArray = getMines(updatedData);
+    const FlagArray = getFlags(updatedData);
+    if (JSON.stringify(mineArray) === JSON.stringify(FlagArray)) {
+
+      setMineCount(0);
+    setGameStatus("You Win.")
+      revealBoard();
+      alert("You Win");
+    }
+  }
+
+  setBoardData(updatedData);
+  setMineCount(mines);
+}
+
+function renderBoard(data) {
+  return data.map((datarow) => {
+    return datarow.map((dataitem) => {
+      return (
+        <div key={dataitem.x * datarow.length + dataitem.y}>
+          <Cell
+            onClick={() => handleCellClick(dataitem.x, dataitem.y)}
+            cMenu={(e) => handleContextMenu(e, dataitem.x, dataitem.y)}
+            value={dataitem}
+          />
+          {(datarow[datarow.length - 1] === dataitem) ? <div className="clear" /> : ""}
+        </div>);
+    })
+  });
+
+}
+
   return (
-    <div>
-      <Cell />
-    </div>
+      <div className="board">
+        <div className="game-info">
+          <span className="info">Mines remaining: {mineCount}</span>
+          <h1 className="info">{gameStatus}</h1>
+        </div>
+        {
+          renderBoard(boardData)
+        }
+      </div>
   );
 }
 
